f734e13dacc29187f58a406fbd29d99aa627eed8
maksim <knyazev.maksim2852@gmail.com>
2025-01-02 21:16:26 +0400

    add page not found handler

--- movie_catalog/movies/views.py
+++ movie_catalog/movies/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from django.urls import Resolver404
 
-# Create your views here.
+
+
+def index(request: HttpRequest):
+    return HttpResponse('Index HTML')
+
+
+def categories(request: HttpRequest, category_id: int):
+    print(request)
+    print(category_id)
+    return HttpResponse(category_id)
+
+
+def page_not_found(request: HttpRequest, exception: Resolver404):
+    return HttpResponseNotFound('<h1>Такой страницы нет!</h1>')
